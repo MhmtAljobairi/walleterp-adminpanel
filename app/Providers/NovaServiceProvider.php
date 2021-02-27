ce9a56fd1d9b9b7750c410a38fefb89270d61d1a
Daly <m.aljobairi@gmail.com>
2021-02-27 13:54:06 +0200

    Fixed issues

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -43,7 +43,8 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     {
         Gate::define('viewNova', function ($user) {
             return in_array($user->email, [
-                'muhamet@walleterp.com','alaa@walleterp.com','nada@walleterp.com','osama@walleterp.com','omar@walleterp.com','ali@walleterp.com'
+                'muhamet@walleterp.com','alaa@walleterp.com','haytham@walleterp.com
+                ','nada@walleterp.com','osama@walleterp.com','omar@walleterp.com','ali@walleterp.com'
             ]);
         });
     }
